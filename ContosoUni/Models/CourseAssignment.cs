f2c0dc6aab206ac7c52c78cb5700bdf30089254a
ac150897@avcol.school.nz <ac150897@avcol.school.nz>
2026-03-13 09:40:12 +1300

    Added the following models: Department.cs, CourseAssignment.cs, OfficeAssignment.cs and Instructor.cs

---
+++ ContosoUni/Models/CourseAssignment.cs
@@ -0,0 +1,17 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUni.Models
+{
+    public class CourseAssignment
+    {
+        [Key]
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
