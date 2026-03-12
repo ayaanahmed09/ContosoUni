37010c4c45525f5e3e0d068c069216ae6f6104b2
ac150897@avcol.school.nz <ac150897@avcol.school.nz>
2026-03-13 09:57:43 +1300

    scaffolded all the new models

---
+++ ContosoUni/Controllers/CourseAssignmentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUni.Models;
+
+namespace ContosoUni.Controllers
+{
+    public class CourseAssignmentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public CourseAssignmentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CourseAssignments
+        public async Task<IActionResult> Index()
+        {
+            var schoolContext = _context.CourseAssignment.Include(c => c.Course);
+            return View(await schoolContext.ToListAsync());
+        }
+
+        // GET: CourseAssignments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseAssignment = await _context.CourseAssignment
+                .Include(c => c.Course)
+                .FirstOrDefaultAsync(m => m.InstructorID == id);
+            if (courseAssignment == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseAssignment);
+        }
+
+        // GET: CourseAssignments/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseID"] = new SelectList(_context.Course, "CourseID", "CourseID");
+            return View();
+        }
+
+        // POST: CourseAssignments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InstructorID,CourseID")] CourseAssignment courseAssignment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(courseAssignment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseID"] = new SelectList(_context.Course, "CourseID", "CourseID", courseAssignment.CourseID);
+            return View(courseAssignment);
+        }
+
+        // GET: CourseAssignments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseAssignment = await _context.CourseAssignment.FindAsync(id);
+            if (courseAssignment == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseID"] = new SelectList(_context.Course, "CourseID", "CourseID", courseAssignment.CourseID);
+            return View(courseAssignment);
+        }
+
+        // POST: CourseAssignments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InstructorID,CourseID")] CourseAssignment courseAssignment)
+        {
+            if (id != courseAssignment.InstructorID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(courseAssignment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseAssignmentExists(courseAssignment.InstructorID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseID"] = new SelectList(_context.Course, "CourseID", "CourseID", courseAssignment.CourseID);
+            return View(courseAssignment);
+        }
+
+        // GET: CourseAssignments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseAssignment = await _context.CourseAssignment
+                .Include(c => c.Course)
+                .FirstOrDefaultAsync(m => m.InstructorID == id);
+            if (courseAssignment == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseAssignment);
+        }
+
+        // POST: CourseAssignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var courseAssignment = await _context.CourseAssignment.FindAsync(id);
+            if (courseAssignment != null)
+            {
+                _context.CourseAssignment.Remove(courseAssignment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseAssignmentExists(int id)
+        {
+            return _context.CourseAssignment.Any(e => e.InstructorID == id);
+        }
+    }
+}
--- ContosoUni/Data/SchoolContext.cs
+++ ContosoUni/Data/SchoolContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Models;
+using ContosoUni.Models;
 
     public class SchoolContext : DbContext
     {
@@ -17,4 +18,12 @@ using ContosoUniversity.Models;
 public DbSet<ContosoUniversity.Models.Course> Course { get; set; } = default!;
 
 public DbSet<ContosoUniversity.Models.Enrollment> Enrollment { get; set; } = default!;
+
+public DbSet<ContosoUni.Models.Department> Department { get; set; } = default!;
+
+public DbSet<ContosoUni.Models.Instructor> Instructor { get; set; } = default!;
+
+public DbSet<ContosoUni.Models.OfficeAssignment> OfficeAssignment { get; set; } = default!;
+
+public DbSet<ContosoUni.Models.CourseAssignment> CourseAssignment { get; set; } = default!;
     }
